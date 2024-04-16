88130c87a1e8237245b942fd32ea26446a530bc1
anaggomes <157487724+anaggomes@users.noreply.github.com>
2024-04-16 11:37:05 +0100

    task 6 solution, error handling and testing; updates endpoints file

--- models/articles.models.js
+++ models/articles.models.js
@@ -24,3 +24,13 @@ exports.insertArticles = () => {
       return rows;
     });
 };
+
+exports.checkArticleExists = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({ rows: article }) => {
+      if (article.length === 0) {
+        return Promise.reject({ status: 404, message: "Not Found" });
+      }
+    });
+};
