dbb409f212e17e59792a016d580347c462808560
anaggomes <157487724+anaggomes@users.noreply.github.com>
2024-04-16 16:29:58 +0100

    solution, test and error handling. updates endpoints file

--- models/comments.models.js
+++ models/comments.models.js
@@ -26,3 +26,17 @@ exports.insertCommentByArticleId = (article_id, username, body) => {
       return comment[0];
     });
 };
+
+exports.removeCommentById = (comment_id) => {
+  return db.query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id]);
+};
+
+exports.checkCommentExists = (comment_id) => {
+  return db
+    .query(`SELECT * FROM comments WHERE comment_id = $1`, [comment_id])
+    .then(({ rows: comment }) => {
+      if (comment.length === 0) {
+        return Promise.reject({ status: 404, message: "Not Found" });
+      }
+    });
+};
